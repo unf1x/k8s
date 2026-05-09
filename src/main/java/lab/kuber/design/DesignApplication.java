bf5308a036e9cadddd030c602a4da9ab4c2f34a7
Maxim Borovskiy <patc3rr@mail.ru>
2026-05-09 11:56:53 +0300

    first commit

---
+++ src/main/java/lab/kuber/design/DesignApplication.java
@@ -0,0 +1,13 @@
+package lab.kuber.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignApplication.class, args);
+	}
+
+}
